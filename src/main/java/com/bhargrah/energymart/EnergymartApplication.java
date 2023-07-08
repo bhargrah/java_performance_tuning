b389207e6eff106b093ab348ae453858be0b9f45
Rahul <rahulbhargava21@gmail.com>
2023-07-08 04:20:40 +0000

    Upgrading Swagger Version

--- src/main/java/com/bhargrah/energymart/EnergymartApplication.java
+++ src/main/java/com/bhargrah/energymart/EnergymartApplication.java
@@ -2,10 +2,27 @@ package com.bhargrah.energymart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
+@EnableSwagger2
 public class EnergymartApplication {
     public static void main(String[] args) {
         SpringApplication.run(EnergymartApplication.class);
     }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select().apis(RequestHandlerSelectors.basePackage("com.bhargrah.energymart.controller"))
+                .paths(regex("/*.*"))
+                .build();
+    }
+
 }
